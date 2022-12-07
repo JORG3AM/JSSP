ec4bbb2909e2abc7a0f5a01eef8e4310f7e19da9
JORG3AM <64338444+JORG3AM@users.noreply.github.com>
2022-12-06 18:15:43 -0600

    Update microgenetic_algorithm.py

--- microgenetic_algorithm.py
+++ microgenetic_algorithm.py
@@ -15,26 +15,28 @@ from readJS import *
 import random
 import matplotlib.pyplot
 
-    #--------------------------Modifiable--------------------------#
-    data = "tai15_15.txt"
-    shape = (15, 15)
-    n_instance = 0
-    #--------------------------------------------------------------#
-    info_df, all_times, all_machines = get_instances(data, shape)
+#--------------------------Modifiable--------------------------#
+data = "tai15_15.txt"
+shape = (15, 15)
+n_instance = 0
+#--------------------------------------------------------------#
+info_df, all_times, all_machines = get_instances(data, shape)
 
-    machines = all_machines[n_instance]
-    times = all_times[n_instance]
+machines = all_machines[n_instance]
+times = all_times[n_instance]
 
-    real_optimal = info_df['Lower bound'][n_instance]
+real_optimal = info_df['Lower bound'][n_instance]
 
-    job = []
-    jobs_data = []
-    for id_m, machine in enumerate(machines):
-        for id_t, time in enumerate(times):
-            job.append((machine[id_t], time[id_t]))
-        jobs_data.append(job)
+job = []
+jobs_data = []
+for id_m, machine in enumerate(machines):
+    for id_t, time in enumerate(times):
+        job.append((machine[id_t], time[id_t]))
+    jobs_data.append(job)
 
 # Individual initialization
+
+
 def createIndividual(all_times, all_machines, n_instance):
     machines = all_machines[n_instance]
     times = all_times[n_instance]
@@ -49,20 +51,24 @@ def createIndividual(all_times, all_machines, n_instance):
         jobs_data.append(job)
     return jobs_data
 
+
 p = createIndividual(1)
 print(p)
 
 # Crossover operator
+
+
 def combine(parentA, parentB, cRate):
   if (random.random() <= cRate):
-    cPoint = numpy.random.randint(1, len(parentA))
+    cPoint = np.random.randint(1, len(parentA))
     #print(cPoint)
-    offspringA = numpy.append(parentA[0:cPoint], parentB[cPoint:])
-    offspringB = numpy.append(parentB[0:cPoint], parentA[cPoint:])
+    offspringA = np.append(parentA[0:cPoint], parentB[cPoint:])
+    offspringB = np.append(parentB[0:cPoint], parentA[cPoint:])
   else:
-    offspringA = numpy.copy(parentA)
-    offspringB = numpy.copy(parentB)
+    offspringA = np.copy(parentA)
+    offspringB = np.copy(parentB)
   return offspringA, offspringB
+
 
 p1 = createIndividual(10)
 p2 = createIndividual(10)
@@ -73,11 +79,14 @@ print(o1)
 print(o2)
 
 # Mutation operator
+
+
 def mutate(individual, mRate):
   for i in range(len(individual)):
     if (random.random() <= mRate):
       individual[i] = not(individual[i])
   return individual
+
 
 p1 = createIndividual(10)
 print(p1)
@@ -85,23 +94,30 @@ mutate(p1, 0.10)
 print(p1)
 
 # Evaluation function
+
+
 def evaluate(individual):
-  return sum(individual)
+  pass
+
 
 p1 = createIndividual(10)
 print(p1)
 print(evaluate(p1))
 
 # Tournament selection
+
+
 def select(population, evaluation, tSize):
-  winner = numpy.random.randint(0, len(population))
+  winner = np.random.randint(0, len(population))
   for i in range(tSize - 1):
-    rival = numpy.random.randint(0, len(population))
+    rival = np.random.randint(0, len(population))
     if (evaluation[rival] > evaluation[winner]):
       winner = rival
   return population[winner]
 
 # Genetic algorithm
+
+
 def geneticAlgorithm(n, pSize, gens, cRate, mRate):
   # Creates the initial population
   population = [None] * pSize
@@ -110,10 +126,10 @@ def geneticAlgorithm(n, pSize, gens, cRate, mRate):
     population[i] = createIndividual(n)
     evaluation[i] = evaluate(population[i])
   # Keeps a record of the best individual found so far
-  index = 0;
+  index = 0
   for i in range(1, pSize):
     if (evaluation[i] > evaluation[index]):
-      index = i;
+      index = i
   bestIndividual = population[index]
   bestEvaluation = evaluation[index]
   # Keeps the information for plotting the performance of the algorithm
@@ -123,7 +139,7 @@ def geneticAlgorithm(n, pSize, gens, cRate, mRate):
   for i in range(gens):
     k = 0
     newPopulation = [None] * pSize
-    
+
     newPopulation[index] = population[index]
     # Crossover
     for j in range(pSize // 2):
@@ -144,14 +160,15 @@ def geneticAlgorithm(n, pSize, gens, cRate, mRate):
         bestIndividual = population[j]
         index = j
       best[i] = bestEvaluation
-      avg[i] = numpy.average(evaluation)
-  matplotlib.pyplot.plot(range(gens), best, label = "Best")
-  matplotlib.pyplot.plot(range(gens), avg, label = "Average")
+      avg[i] = np.average(evaluation)
+  matplotlib.pyplot.plot(range(gens), best, label="Best")
+  matplotlib.pyplot.plot(range(gens), avg, label="Average")
   matplotlib.pyplot.legend()
   matplotlib.pyplot.title("GA Run")
   matplotlib.pyplot.show()
   # Returns the best individual found so far
   return bestIndividual, bestEvaluation
+
 
 # Tests
 solution, eval = geneticAlgorithm(100, 8, 100, 1.0, 0.01)
